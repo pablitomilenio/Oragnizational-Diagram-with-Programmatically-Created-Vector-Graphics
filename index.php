0eebfdca8234aabb06cc49963c79756512e2db40
pablitomillenios <65786238+pablitomillenios@users.noreply.github.com>
2020-12-12 23:28:28 +0100

    functional version 1

--- index.php
+++ index.php
@@ -12,10 +12,11 @@
 <body>
 Please Drag + Drop to crete the Org-Chart
 
-<div id="div1" ondrop="drop(event);addBox();" ondragover="allowDrop(event)"></div>
+<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 
 <div id='subord_list'>
-<div id='anEmployee' ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+<div id='employee1' class='anEmployee' ondrop="drop(event); addBox();" ondragover="allowDrop(event)"></div>
+<div class='anEmployee' ondrop="drop(event); addBox();" ondragover="allowDrop(event)"></div>
 </div>
 
 <img id="drag1" src="img/oneEmployee.png" draggable="true" ondragstart="drag(event)" width="180">
--- script/displayLogic.js
+++ script/displayLogic.js
@@ -1,3 +1,6 @@
 function addBox() {
-    
+    $("#employee1")
+    .clone()
+    .removeAttr('id')
+    .appendTo("#subord_list");
 }
