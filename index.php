acd7edd3d325455e70d10074d73a80aebcc72bd5
pablitomillenios <65786238+pablitomillenios@users.noreply.github.com>
2020-12-12 22:00:03 +0100

    DragDrop essential implemented

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="/css/styles.css">
+<link rel="stylesheet" href="css/styles.css">
+<script src="script/dragdrop.js"></script>
 <title>Title of the document</title>
 </head>
 
 <body>
 The content of the document......
+
+<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+
+<img id="drag1" src="img/img_logo.gif" draggable="true" ondragstart="drag(event)" width="336" height="69">
+
 </body>
 
 </html>
